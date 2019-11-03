949e0a7bc8f90ba58cee4fc1a96702fd6fdd9413
hafizsameed <hafizsameed42@gmail.com>
2019-11-03 11:43:50 +0500

    news page done

--- src/Screens/new.js
+++ src/Screens/new.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ServiceCard from '../Components/serviceCards'
 import './news.css'
+import {shortCourse,computerCourse} from '../config/courses'
 
 function News() {
 
@@ -16,58 +17,286 @@ function News() {
             <div className='news-sec-block'>
             <div className='news-sidebar'>
             <div className='mati-heading'>Menu</div>
-                <a className='news-links' href='#'>PIPFA Notifications & Circulars </a>
-                <a className='news-links' href='#'>Achievements</a>
-                <a className='news-links' href='#'>Tender and Jobs Notifications</a>
+                <a className='news-links' href='#pipfa'>PIPFA Notifications & Circulars </a>
+                <a className='news-links' href='#achievement'>Achievements</a>
+                <a className='news-links' href='#tender'>Tender and Jobs Notifications</a>
                 <div className='mati-heading'>MATI Courses  Calender</div>
-                <a className='news-links' href='#'>Short Courses</a>
-                <a className='news-links' href='#'>Computer Courses</a>
+                <a className='news-links' href='#shortCourses'>Short Courses</a>
+                <a className='news-links' href='#compCourses'>Computer Courses</a>
                 
             </div>
-            <div className='circulars-div'>
+            <div id='pipfa' className='circulars-div'>
                 <div className='circulars-heading'>
                     PIPFA Notifications & Circulars
                 </div>
                 <div className='circulars'>
                     <div className='table-headings'>
+                        <div className='sno-head'>S.No.</div>
                         <div className='notification-head'>Notifications</div>
                         <div className='date-head'>date</div>
                     </div>
                     <div className='circular'>
+                        <div className='sno'>1</div>
                         <div className='noti-text'>
-                            Text
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
                         </div>
                         <div className='noti-date'>
-                            date
+                            11/11/2019
+                        </div>
+                    </div>
+
+                    <div className='circular'>
+                        <div className='sno'>1</div>
+                        <div className='noti-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
+                        </div>
+                        <div className='noti-date'>
+                            11/11/2019
+                        </div>
+                    </div>
+
+                    <div className='circular'>
+                        <div className='sno'>1</div>
+                        <div className='noti-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
+                        </div>
+                        <div className='noti-date'>
+                            11/11/2019
                         </div>
                     </div>
                 </div>
 
             </div>
             </div>
+            <div className='news-third-block'>
+            <div className='news-sidebar'>
+                <div className='useful-links-div'>
+                    <div className='mati-heading'>Useful Links</div>
+                    <div className='useful-links'>
+                        <a className='useful-news-links' href='http://www.pakistan.gov.pk'>Government of Pakistan</a>
+                        <a className='useful-news-links' href='http://www.agp.gov.pk'>Auditor General of Pakistan</a>
+                        <a className='useful-news-links' href='http://www.mod.gov.pk'>Ministry of Defence</a>
+                        <a className='useful-news-links' href='http://www.finance.gov.pk'>Ministry of Finance</a>
+                        <a className='useful-news-links' href='http://www.establishment.gov.pk'>Establishment Division</a>
+                    </div>
+
+            </div>
+
+            <div className='cma-links-div'>
+                    <div className='mati-heading'>CMA Links</div>
+                    <div className='useful-links'>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/#'>Controller Military Account (RC) Rawalpindi</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/op'>Controller Military Account (OP) Rawalpindi</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/cmap'>Controller Military Account (pension) Lahore</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/cladp'>Controller Local Audit (DP) Rawalpindi</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/caaf'>Controller Accounts of AirForce (CAAF) Peshawar</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/#'>Controller Local Audit (DS) Lahore</a>
+                        <a className='useful-news-links' href='http://www.pmad.gov.pk/mc'>Controller Military Accounts (MC) Multan</a>
+                    </div>
+
+            </div>
+    
+                
+            </div>
+            <div id='tender' className='circulars-div'>
+                <div className='circulars-heading'>
+                    Tender & Job Notifications
+                </div>
+                <div className='circulars'>
+                    <div className='table-headings'>
+                        <div className='sno-head'>S.No.</div>
+                        <div className='notification-head'>Notifications</div>
+                        <div className='date-head'>date</div>
+                    </div>
+                    <div className='circular'>
+                        <div className='sno'>1</div>
+                        <div className='noti-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
+                        </div>
+                        <div className='noti-date'>
+                            11/11/2019
+                        </div>
+                    </div>
+
+                    <div className='circular'>
+                        <div className='sno'>1</div>
+                        <div className='noti-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
+                        </div>
+                        <div className='noti-date'>
+                            11/11/2019
+                        </div>
+                    </div>
+
+                    <div className='circular'>
+                        <div className='sno'>1</div>
+                        <div className='noti-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae.
+                        </div>
+                        <div className='noti-date'>
+                            11/11/2019
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            </div>
+
             <div className='courses-div'>
+            <div className='courses-heading-div'>
+            <div className = "news-icon-div">
+                    <i class="fa fa-check-circle  fa-5x" aria-hidden="true"></i>
+                </div>
                 <div className='courses-heading'>
                     Course Plan - 2019
                 </div>
                 <div className='courses-subheading'>
                     Rawalpindi, Lahore, Multan, Karachi, Wah, Peshawar, Quetta
                 </div>
-                <div className='shortc-div'>
+            </div>
+                <div id='shortCourses' className='shortc-div'>
                     <div className='shortc-head'>
                         Short Courses
                     </div>
                     <div className='shortc'>
-
+                <div className='shortc-table-heads'>
+                    <div className='shortc-sno-head'>
+                    S. No.
+                    </div>
+                    <div className='shortc-cname-head'>
+                    Course Name
+                    </div>
+                    <div className='shortc-month-head'>
+                    Month
+                    </div>
+                    <div className='shortc-level-head'>
+                    Level
+                    </div>
+                    <div className='shortc-duration-head'>
+                    Duration
+                    </div>
+                    <div className='shortc-date-head'>
+                    Dates
+                    </div>
+                </div>
+                {shortCourse.map((e)=>{
+                   return <div className='shortc-table-items'>
+                    <div className='shortc-sno'>
+                    {e.sno}
+                    </div>
+                    <div className='shortc-cname'>
+                    {e.name}
+                    </div>
+                    <div className='shortc-month'>
+                    {e.month}
+                    </div>
+                    <div className='shortc-level'>
+                    {e.level}
+                    </div>
+                    <div className='shortc-duration'>
+                    {e.duration}
+                    </div>
+                    <div className='shortc-date'>
+                    {e.dates}
+                    </div>
+                </div>
+                })}
                     </div> 
 
                 </div>
-                <div className='compc-div'>
+                <div id='compCourses' className='compc-div'>
                     <div className='compc-head'>Computer Courses</div>
                     <div className='compc'>
-                        hello world
+                    <div className='shortc-table-heads'>
+                    <div className='shortc-sno-head'>
+                    S. No.
+                    </div>
+                    <div className='shortc-cname-head'>
+                    Course Name
+                    </div>
+                    <div className='shortc-month-head'>
+                    Month
+                    </div>
+                    <div className='shortc-level-head'>
+                    Level
+                    </div>
+                    <div className='shortc-duration-head'>
+                    Duration
+                    </div>
+                    <div className='shortc-date-head'>
+                    Dates
+                    </div>
+                    <div className='compc-station-head'>
+                    Station
+                    </div>
+                </div>
+                {computerCourse.map((e)=>{
+                   return <div className='shortc-table-items'>
+                    <div className='shortc-sno'>
+                    {e.sno}
+                    </div>
+                    <div className='shortc-cname'>
+                    {e.name}
+                    </div>
+                    <div className='shortc-month'>
+                    {e.month}
+                    </div>
+                    <div className='shortc-level'>
+                    {e.level}
+                    </div>
+                    <div className='shortc-duration'>
+                    {e.duration}
+                    </div>
+                    <div className='shortc-date'>
+                    {e.dates}
+                    </div>
+                    <div className='compc-station'>
+                        {e.station}
+                        </div>
+                </div>
+                })}
                     </div>    
                 </div>
             </div>
+
+            <div id='achievement' className='achievements-div'>
+                <div className='achievements-heading'>
+                    Achievements
+                </div>
+                <div className='achievements'>
+
+                <div className='card'>
+                <div className='text-div'>
+                    <div className='text-title'>Tree Plantation</div>
+                    <div className='text-detail'>MAG Orders for Tree Plantation. . . .</div>
+                </div>
+                <div className='img-div'>
+                    <img className='img' src='https://static.wixstatic.com/media/0a8802f2cb234551bc855be44c0f34de.jpg/v1/fill/w_460,h_320,al_c,q_80,usm_0.66_1.00_0.01/Experts%20Panel.webp' />
+                </div>
+           </div>
+
+           <div className='card'>
+                <div className='text-div'>
+                    <div className='text-title'>Tree Plantation</div>
+                    <div className='text-detail'>MAG Orders for Tree Plantation. . . .</div>
+                </div>
+                <div className='img-div'>
+                    <img className='img' src='https://static.wixstatic.com/media/0a8802f2cb234551bc855be44c0f34de.jpg/v1/fill/w_460,h_320,al_c,q_80,usm_0.66_1.00_0.01/Experts%20Panel.webp' />
+                </div>
+           </div>
+
+           <div className='card'>
+                <div className='text-div'>
+                    <div className='text-title'>Tree Plantation</div>
+                    <div className='text-detail'>MAG Orders for Tree Plantation. . . .</div>
+                </div>
+                <div className='img-div'>
+                    <img className='img' src='https://static.wixstatic.com/media/0a8802f2cb234551bc855be44c0f34de.jpg/v1/fill/w_460,h_320,al_c,q_80,usm_0.66_1.00_0.01/Experts%20Panel.webp' />
+                </div>
+           </div>
+                </div>
+            </div>
+
         </div>
     )
 }
---
+++ src/config/courses.js
@@ -0,0 +1,244 @@
+ const shortCourse=[
+    {
+        sno:'1',
+        name:'Noting & Drafting Skills (Workshop)',
+        level:'B 11 - 18',
+        duration:'4 days',
+        dates:'15-01-19 to 18-01-19',
+        month:'jan'
+    },
+    {
+        sno:'2',
+        name:'Revised Leave Rules (Workshop)',
+        level:'B 11 - 18',
+        duration:'3 days',
+        dates:'23-01-19 to 24-01-19',
+        month:'jan'
+    },
+    {
+        sno:'3',
+        name:'Preparation of Annual Accounts',
+        level:'B 11 - 18',
+        duration:'2 days',
+        dates:'06-02-19 to 07-02-19',
+        month:'feb'
+    },
+    {
+        sno:'4',
+        name:'Procurement and Contract Auditing',
+        level:'B 11 - 18',
+        duration:'5 days',
+        dates:'11-02-19 to 15-02-19',
+        month:'feb'
+    },
+    {
+        sno:'5',
+        name:'Primary Course in Accounting Skills',
+        level:'B 11 - 18',
+        duration:'3 days',
+        dates:'19-02-19 to 21-02-19',
+        month:'feb'
+    },
+    {
+        sno:'6',
+        name:'Course on Local Audit',
+        level:'B 11 - 18',
+        duration:'5 days',
+        dates:'04-03-19 to 08-03-19',
+        month:'march'
+    },
+    {
+        sno:'7',
+        name:'Course on Performance Evaluation Report (PERs/ACRs)',
+        level:'B 17 - 18',
+        duration:'3 days',
+        dates:'19-03-19 to 21-03-19',
+        month:'march'
+    },
+    {
+        sno:'8',
+        name:'Communication Skills',
+        level:'B 11 - 18',
+        duration:'3 days',
+        dates:'25-03-19 to 27-03-19',
+        month:'march'
+    },
+    {
+        sno:'9',
+        name:'Travelling Allowances Rules',
+        level:'B 11 - 18',
+        duration:'5 days',
+        dates:'01-04-19 to 05-04-19',
+        month:'april'
+    },
+    {
+        sno:'10',
+        name:'Short Course for Drivers',
+        level:'B-5 - 7',
+        duration:'1 days',
+        dates:'10-04-19 to 10-04-19',
+        month:'april'
+    },
+    {
+        sno:'11',
+        name:'Pensive Rules (Civilian /Defence Sevices)',
+        level:'B 11 - 18',
+        duration:'5 days',
+        dates:'15-04-19 to 19-04-19',
+        month:'april'
+    },
+    {
+        sno:'12',
+        name:'Course on works (MES)',
+        level:'B 11 - 18',
+        duration:'3 days',
+        dates:'22-04-19 to 24-04-19',
+        month:'april'
+    },
+    {
+        sno:'13',
+        name:'An overview of New Accounting Model (NAM)',
+        level:'B 11 - 18',
+        duration:'2 days',
+        dates:'02-05-19 to 03-05-19',
+        month:'may'
+    },
+    {
+        sno:'14',
+        name:'Orientation Course for Naval Officers',
+        level:'Lt./Lt.Cdr',
+        duration:'4 days',
+        dates:'21-05-19 to 24-05-19',
+        month:'may'
+    },
+    {
+        sno:'15',
+        name:'Audit Report Writing',
+        level:'B 11 - 18',
+        duration:'2 days',
+        dates:'12-06-19 to 13-05-19',
+        month:'june'
+    },
+    {
+        sno:'16',
+        name:'Budget and Control over Expenditure',
+        level:'B 11 - 18',
+        duration:'2 days',
+        dates:'17-06-19 to 18-05-19',
+        month:'june'
+    },
+    {
+        sno:'17',
+        name:'Short Course for Naib Qasid',
+        level:'B 1 - 4',
+        duration:'1 days',
+        dates:'19-06-19 to 19-05-19',
+        month:'june'
+    },
+    {
+        sno:'18',
+        name:'Training of UAs/AUAs (RSPA)',
+        level:'B 11 - 18',
+        duration:'5 days',
+        dates:'24-06-19 to 28-05-19',
+        month:'june'
+    },
+
+ ]
+
+
+ const computerCourse=[
+    {
+        sno:'1',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'14-01-19 to 25-01-19',
+        month:'jan',
+        station:'All MATIs'
+    },
+    {
+        sno:'2',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'11-02-19 to 22-02-19',
+        month:'feb',
+        station:'All MATIs'
+    },
+    {
+        sno:'3',
+        name:'Course on Advance Excel',
+        level:'B 11 - 18',
+        duration:'05 days',
+        dates:'11-03-19 to 15-02-19',
+        month:'march',
+        station:'All MATIs'
+    },
+    {
+        sno:'4',
+        name:'Audit Command Language (ACL)',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'08-04-19 to 19-04-19',
+        month:'april',
+        station:'MATI Rawalpindi Only'
+    },
+    {
+        sno:'5',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'15-04-19 to 26-04-19',
+        month:'april',
+        station:'All MATIs except Rwp'
+    },
+    {
+        sno:'6',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'13-05-19 to 24-05-19',
+        month:'may',
+        station:'All MATIs'
+    },
+    {
+        sno:'7',
+        name:'Course on Advance Excel',
+        level:'B 11 - 18',
+        duration:'05 days',
+        dates:'17-06-19 to 21-06-19',
+        month:'june',
+        station:'All MATIs'
+    },
+    {
+        sno:'8',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'15-07-19 to 24-05-19',
+        month:'july',
+        station:'All MATIs'
+    },
+    {
+        sno:'9',
+        name:'Office Automation',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'19-08-19 to 30-08-19',
+        month:'aug',
+        station:'All MATIs except Rwp'
+    },
+    {
+        sno:'10',
+        name:'Audit Command Language (ACL)',
+        level:'B 11 - 18',
+        duration:'10 days',
+        dates:'19-08-19 to 30-08-19',
+        month:'aug',
+        station:'MATI Rawalpindi only'
+    },
+ ]
+
+
+ export {shortCourse,computerCourse};
